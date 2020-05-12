e1210af2d8236be1b7f5c1174d4eb653fdeb0f46
Renat Gubaev <>
2020-05-12 11:37:31 +0300

    no message

--- main.ts
+++ main.ts
@@ -12,52 +12,6 @@ async function bootstrap(): Promise<void> {
 
     let example = new Example(logger, settings);
     await example.initialize(connector);
-    // await transferUSDTFromBobToAlice();
-    // await transferPDTFromAliceToBob();
 }
 
 bootstrap();
-
-/*
-async function showBalances(): Promise<void> {
-    for (let token of [pdt, usdt]) {
-        logger.log(`Total supply: ${await token.totalSupply()} ${token.name}`);
-        for (let user of [alice, bob]) {
-            logger.log(`${user.name}: ${await token.balanceOf(user.address)} ${token.name}`);
-        }
-        console.log();
-    }
-
-    logger.log(`${usdt.name}: allowed ${await pdt.allowedBalanceOf(bob.address, usdt.name)} ${pdt.name}`);
-}
-
-async function transferUSDTFromBobToAlice(): Promise<void> {
-    let amount = '100';
-    if (await usdt.transfer(alice.address, amount, bob)) {
-        logger.log(`${bob.name} transfered ${alice.name} ${amount} ${usdt.name}`);
-    }
-    console.log();
-}
-
-// Transfer PDT tokens from Alice to Bob
-async function transferPDTFromAliceToBob(): Promise<void> {
-    let amount = '5';
-    if (await pdt.transfer(bob.address, amount, alice)) {
-        logger.log(`${alice.name} transfered ${bob.name} ${amount} ${pdt.name}`);
-    }
-    console.log();
-}
-
-async function allowUSDTtoPDT(): Promise<void> {
-    let key = '0xFF';
-
-    let id = await usdt.swapBegin(pdt.name, '10', key, bob);
-    logger.log(`${bob.name} started swap ${id}. Waiting 30 seconds...`);
-
-    await PromiseHandler.delay(30 * DateUtil.MILISECONDS_SECOND);
-    console.log(await pdt.swapDone(id, key));
-    console.log();
-    logger.log(`${usdt.name}: allowed ${await pdt.allowedBalanceOf(bob.address, usdt.name)} ${pdt.name}`);
-}
-
-*/
